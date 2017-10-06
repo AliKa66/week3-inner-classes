d82f9cde8dc43a35a4f1e5d784869050f82ff102
AliKa66 <11500638@student.pxl.be>
2017-10-06 17:29:52 +0200

    Opdrachte2: Create innerclasses in HorrorShow

--- src/be/pxl/week3/opdracht2/HorrorShow.java
+++ src/be/pxl/week3/opdracht2/HorrorShow.java
@@ -4,6 +4,37 @@ public class HorrorShow {
 
 	public static void main(String[] args) {
 
-	}
+		class Vampire implements Monster {
+			@Override
+			public void menace() {
+				System.out.println("I’ll drink your blood!!");
+			}
+		}
 
+		class Werewolf implements DangerousMonster {
+
+			@Override
+			public void menace() {
+				System.out.println("I’ll destroy you.");
+			}
+
+			@Override
+			public void destroy(Monster monster) {
+				monster.menace();
+				System.out.println("Big fight...");
+				System.out.println(this.getClass().getName());
+				System.out.println("killed one");
+				System.out.println(monster.getClass().getName());
+			}
+		}
+		Werewolf werewolf = new Werewolf();
+		Vampire vampire = new Vampire();
+		werewolf.destroy(vampire);
+		werewolf.destroy(new Monster() {
+			@Override
+			public void menace() {
+				System.out.println("You smell like wet dog");
+			}
+		});
+	}
 }
